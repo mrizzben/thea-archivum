76121f24e6a9ea7945fd16ec193af5b41a864fea
Rizz <mnoerrahman@unknownDev.local>
2023-05-29 16:23:43 +0700

    feat: reformat templates

--- teascore.py
+++ teascore.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pytz
 
 flavor_wheel = {
     'Sweetness': ['Honey', 'Caramel', 'Sugar', 'Maple Syrup', 'Marshmallow', 'Agave'],
@@ -14,87 +15,216 @@ flavor_wheel = {
     'Woody': ['Cedar', 'Oak', 'Pine', 'Sandalwood', 'Cypress', 'Mahogany'],
     'Other': ['Biscuit', 'Molasses', 'Tobacco', 'Leather', 'Burnt Sugar', 'Toasted Sesame']
 }
+dry_leaf_appearance_attributes = [
+    'Twisted',
+    'Curled',
+    'Rolled',
+    'Tightly Rolled',
+    'Loosely Rolled',
+    'Long and Thin',
+    'Short and Stout',
+    'Needle-like',
+    'Balled',
+    'Wiry',
+    'Flat',
+    'Fluffy',
+    'Spiral',
+    'Spear-shaped',
+    'Unfolded',
+    'Crushed',
+    'Whole',
+    'Broken',
+    'Tippy',
+    'Golden',
+    'Silver',
+    'Green',
+    'Black',
+    'Brown',
+    'Mixed Colors',
+    'Mottled',
+    'Even',
+    'Uneven',
+    'Uniform',
+    'Assorted Sizes',
+    'Homogeneous',
+    'Delicate',
+    'Crinkled',
+    'Tangled',
+    'Lustrous',
+    'Dull',
+    'Brittle',
+    'Dry',
+    'Moist',
+    'Tender',
+    'Stiff',
+    'Pristine',
+    'Clean',
+    'Dirty',
+    'Speckled',
+    'Speck-free',
+]
+
+wet_leaf_appearance_attributes = [
+    'Expanded',
+    'Unfurled',
+    'Plumped',
+    'Soft',
+    'Tender',
+    'Supple',
+    'Limp',
+    'Glossy',
+    'Shiny',
+    'Bright',
+    'Dull',
+    'Wilted',
+    'Soggy',
+    'Evenly Wet',
+    'Partially Wet',
+    'Waterlogged',
+    'Dripping',
+    'Steamed',
+    'Steeped',
+    'Infused',
+    'Saturated',
+    'Bright Green',
+    'Dark Green',
+    'Olive Green',
+    'Brown',
+    'Black',
+    'Red',
+    'Mixed Colors',
+    'Uniform',
+    'Mottled',
+    'Strands',
+    'Leaves',
+    'Buds',
+    'Tendrils',
+    'Stalks',
+    'Sprouts',
+    'Veins',
+    'Separated',
+    'Clumped',
+    'Delicate',
+    'Fragile',
+    'Intact',
+    'Shredded',
+    'Bruised',
+    'Mangled',
+    'Torn',
+    'Infusion Color',
+    'Transparent',
+    'Opaque',
+    'Clear',
+    'Cloudy',
+    'Filtered',
+    'Unfiltered',
+    'Visibly Extracted',
+    'Well-Steeped',
+]
 
 color = ['Pale Yellow / Green', 'Green', 'Yellow', 'Orange', 'Red', 'Brown']
 clarity = ['Clear', 'Dark', 'Murky']
 texture = ['Heavy', 'Metallic', 'Thick', 'Creamy', 'Full', 'Empty', 'Bright', 'Soft', 'Light', 'Mediums']
 after_quality = ['Clean', 'Lingering', 'Empty']
+country = list(pytz.country_names.values())
+country.insert(0, 'N/A')
+types = [
+    'Black Tea',
+    'Green Tea',
+    'Purple Tea',
+    'White Tea',
+    'Yellow Tea',
+    'Oolong Tea',
+    'Pu-erh Tea',
+    'Dark / Post-Fermented Tea',
+    'Tisane / Infusion',
+    'Blend']
 
 def main():
     st.title('Tea Tasting Score Sheet')
 
-    taster_name = st.text_input('Taster\'s Name')
-    date = st.date_input('Date')
-    tea_name = st.text_input('Tea Name')
+    with st.container():
+        col1, col2 = st.columns(2)
+        taster_name = col1.text_input('Taster\'s Name')
+        date = col2.date_input('Date')
+        tea_name = col1.text_input('Tea Name')
+        tea_type = col2.selectbox('Tea Type', types)
+
+    with st.expander('Add Further Details'):
+        region = st.selectbox('Country of Origin', country)
 
     with st.container():
 
         col1, col2 = st.columns(2)
 
         col1.header('Dry Leaf Evaluation')
-        dry_leaf_appearance = col1.text_input('Dry Appearance')
-        dry_leaf_aroma = col1.text_input('Dry Aroma')
+        dry_leaf_appearance = col1.multiselect('Dry Appearance', dry_leaf_appearance_attributes)
 
         col2.header('Wet Leaf Evaluation')
-        wet_leaf_appearance = col2.text_input('Wet Appearance')
-        wet_leaf_aroma = col2.text_input('Wet Aroma')
+        wet_leaf_appearance = col2.multiselect('Wet Appearance', wet_leaf_appearance_attributes)
 
     st.header('Liquor Evaluation')
-    liquor_color = st.select_slider('Liqour Color', color)
-    liqour_clarity = st.radio('Liqour Clarity', clarity)
+    liquor_color = st.radio('Liqour Color', color, horizontal=True)
+    liqour_clarity = st.radio('Liqour Clarity', clarity, horizontal=True)
     liquor_intensity = st.number_input('Aroma Intensity', 1, 5)
+    st.caption('1 : Muted / Very Slight - 5 : Very Intense')
     liqour_body = st.multiselect('Body (Viscosity, Weight)', texture)
+    st.caption('You may choose more than one attributes')
 
-    st.header('Flavor Intensity')
-    st.write('1: Muted / None - 5 : Very Intense')
+    st.subheader('Flavor Intensity')
+    st.caption('0: None - 1 : Muted / Very Slight - 5 : Very Intense')
     with st.container():
         col1, col2, col3 = st.columns(3)
-        sweetness = col1.number_input('Sweetness', min_value=1, max_value=5)
-        bitterness = col2.number_input('Bitterness', min_value=1, max_value=5)
-        astringency = col3.number_input('Astringency', min_value=1, max_value=5)
-        floral = col1.number_input('Floral', min_value=1, max_value=5)
-        fruity = col2.number_input('Fruity', min_value=1, max_value=5)
-        earthy = col3.number_input('Earthy', min_value=1, max_value=5)
-        herbal = col1.number_input('Herbal', min_value=1, max_value=5)
-        spicy = col2.number_input('Spicy', min_value=1, max_value=5)
-        vegetal = col3.number_input('Vegetal', min_value=1, max_value=5)
-        nutty = col1.number_input('Nutty', min_value=1, max_value=5)
-        woody = col2.number_input('Woody', min_value=1, max_value=5)
-        umami = col3.number_input('Umami', min_value=1, max_value=5)
+        sweetness = col1.number_input('Sweetness', min_value=0, max_value=5)
+        bitterness = col2.number_input('Bitterness', min_value=0, max_value=5)
+        astringency = col3.number_input('Astringency', min_value=0, max_value=5)
+        floral = col1.number_input('Floral', min_value=0, max_value=5)
+        fruity = col2.number_input('Fruity', min_value=0, max_value=5)
+        earthy = col3.number_input('Earthy', min_value=0, max_value=5)
+        herbal = col1.number_input('Herbal', min_value=0, max_value=5)
+        spicy = col2.number_input('Spicy', min_value=0, max_value=5)
+        vegetal = col3.number_input('Vegetal', min_value=0, max_value=5)
+        nutty = col1.number_input('Nutty', min_value=0, max_value=5)
+        woody = col2.number_input('Woody', min_value=0, max_value=5)
+        umami = col3.number_input('Umami', min_value=0, max_value=5)
 
     selected_flavors = {}
 
-    st.header('Flavour Attributes')
-
+    st.subheader('Flavour Attributes')
+    st.caption('You may choose more than one attributes per flavour profile')
     for attribute, options in flavor_wheel.items():
         selected = st.multiselect(attribute, options)
         selected_flavors[attribute] = selected
 
-    st.header('Selected Flavors')
+    st.subheader('Selected Flavors')
     st.write(selected_flavors)
 
-    st.header('Aftertaste')
+    st.subheader('Aftertaste')
     with st.container():
         col1, col2 = st.columns(2)
         aftertaste_duration = col1.number_input('Duration', 1, 5)
-        col1.write('1 : None / Short - 5: Lingering / Very Long')
+        col1.caption('1 : None / Very Short - 5: Lingering / Very Long')
         aftertaste_quality = col2.number_input('Quality', 1, 5)
-        col2.write('1 : Acceptable - 5 : Outstanding')
+        col2.caption('1 : Acceptable - 5 : Outstanding')
 
-    st.header('Mouthfeel')
-    st.write('1: Muted / None - 5 : Very Intense')
+    st.subheader('Mouthfeel')
+    st.caption('0: None - 1 : Muted / Very Slight - 5 : Very Intense')
     with st.container():
         col1, col2 = st.columns(2)
-        smoothness = col1.number_input('Smoothness', 1, 5)
-        thickness = col2.number_input('Thickness', 1, 5)
-        creaminess = col1.number_input('Creaminess', 1, 5)
-        dryness = col2.number_input('Dryness', 1, 5)
+        smoothness = col1.number_input('Smoothness', 0, 5)
+        thickness = col2.number_input('Thickness', 0, 5)
+        creaminess = col1.number_input('Creaminess', 0, 5)
+        dryness = col2.number_input('Dryness', 0, 5)
 
     st.header('Overall Evaluation')
-    balance = st.text_input('Balance (Flavors, Aromas, and Body)')
-    complexity = st.text_input('Complexity')
-    cleanliness = st.text_input('Cleanliness (Lack of Off-flavors)')
-    preference = st.text_input('Personal Preference')
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+        balance = col1.number_input('Balance', 1, 5)
+        col1.caption('Balance of Flavors, Aromas, and Body')
+        complexity = col2.number_input('Complexity', 1, 5)
+        col2.caption('Nuance of Flavours, Textures and Aromas')
+        cleanliness = col3.number_input('Cleanliness', 1, 5)
+        col3.caption('Lack of Unpleasant Off-flavors')
 
     st.header('Additional Notes')
     additional_notes = st.text_area('Additional Notes', height=200)
