b462da1ec11639eea0497e641ee04e9db3818f64
Rizz <26541542+mrizzben@users.noreply.github.com>
2023-05-29 20:24:17 +0700

    feat: update to use attributes from jsons

--- teascore.py
+++ teascore.py
@@ -2,19 +2,16 @@ import streamlit as st
 import pytz
 from src.dataloader.load import json_load
 
-flavor_wheel = json_load('conf/flavour_wheel.json')
-
-dry_leaf_appearance_attributes = json_load('conf/dry_appearance.json')
-
-wet_leaf_appearance_attributes = json_load('conf/wet_appearance.json')
-
-color = ['Pale Yellow / Green', 'Green', 'Yellow', 'Orange', 'Red', 'Brown']
-clarity = ['Clear', 'Dark', 'Murky']
-texture = ['Heavy', 'Metallic', 'Thick', 'Creamy', 'Full', 'Empty', 'Bright', 'Soft', 'Light', 'Mediums']
-after_quality = ['Clean', 'Lingering', 'Empty']
-country = list(pytz.country_names.values())
-country.insert(0, 'N/A')
-types = json_load('conf/tea_types.json')
+FLAVOR_WHEEL = json_load('conf/flavour_wheel.json')
+DRY_LEAF_ATTRS = json_load('conf/dry_appearance.json')
+WET_LEAF_ATTRS = json_load('conf/wet_appearance.json')
+TEXTURE = json_load('conf/texture.json')
+TYPES = json_load('conf/tea_types.json')
+COUNTRY = list(pytz.country_names.values())
+COUNTRY.insert(0, 'N/A')
+COLOR = ['Pale Yellow / Green', 'Green', 'Yellow', 'Orange', 'Red', 'Brown']
+CLARITY = ['Clear', 'Dark', 'Murky']
+PEKOE_GRADES = json_load('conf/pekoe_grades.json')
 
 
 def main():
@@ -25,31 +22,47 @@ def main():
         taster_name = col1.text_input('Taster\'s Name')
         date = col2.date_input('Date')
         tea_name = col1.text_input('Tea Name')
-        tea_type = col2.selectbox('Tea Type', types)
+        tea_type = col2.selectbox('Tea Type', TYPES)
+        if tea_type == 'Black Tea':
+            grade = st.selectbox('Pekoe Grades', PEKOE_GRADES, index=0)
+        else:
+            grade = 'N/A'
+
 
     with st.expander('Add Further Details'):
-        region = st.selectbox('Country of Origin', country)
+        region = st.selectbox('Country of Origin', COUNTRY)
+        area = st.text_input('Region / Area')
+        processor = st.text_input('Processor')
+        plantation = st.text_input('Plantation')
+        distributor = st.text_input('Distributor')
+        
 
     with st.container():
 
         col1, col2 = st.columns(2)
 
         col1.header('Dry Leaf Evaluation')
-        dry_leaf_appearance = col1.multiselect('Dry Appearance', dry_leaf_appearance_attributes)
+        dry_leaf_appearance = col1.multiselect(
+            'Dry Appearance',
+            DRY_LEAF_ATTRS
+        )
 
         col2.header('Wet Leaf Evaluation')
-        wet_leaf_appearance = col2.multiselect('Wet Appearance', wet_leaf_appearance_attributes)
+        wet_leaf_appearance = col2.multiselect(
+            'Wet Appearance',
+            WET_LEAF_ATTRS
+        )
 
     st.header('Liquor Evaluation')
-    liquor_color = st.radio('Liqour Color', color, horizontal=True)
-    liqour_clarity = st.radio('Liqour Clarity', clarity, horizontal=True)
+    liquor_color = st.radio('Liqour Color', COLOR, horizontal=True)
+    liqour_clarity = st.radio('Liqour Clarity', CLARITY, horizontal=True)
     liquor_intensity = st.number_input('Aroma Intensity', 1, 5)
-    st.caption('1 : Muted / Very Slight - 5 : Very Intense')
-    liqour_body = st.multiselect('Body (Viscosity, Weight)', texture)
+    st.caption('0 : None - 1 : Muted - 2 : Slight - 3 : Mild - 4 : Intense - 5 : Very Intense')
+    liqour_body = st.multiselect('Body (Viscosity, Weight)', TEXTURE)
     st.caption('You may choose more than one attributes')
 
     st.subheader('Flavor Intensity')
-    st.caption('0: None - 1 : Muted / Very Slight - 5 : Very Intense')
+    st.caption('0 : None - 1 : Muted - 2 : Slight - 3 : Mild - 4 : Intense - 5 : Very Intense')
     with st.container():
         col1, col2, col3 = st.columns(3)
         sweetness = col1.number_input('Sweetness', min_value=0, max_value=5)
@@ -73,7 +86,7 @@ def main():
     num_columns = 2
 
     columns = st.columns(num_columns)
-    for i, (attribute, options) in enumerate(flavor_wheel.items()):
+    for i, (attribute, options) in enumerate(FLAVOR_WHEEL.items()):
         selected = columns[i % num_columns].multiselect(attribute, options, key=i)
         selected_flavors[attribute] = selected
 
@@ -89,7 +102,7 @@ def main():
         col2.caption('1 : Acceptable - 5 : Outstanding')
 
     st.subheader('Mouthfeel')
-    st.caption('0: None - 1 : Muted / Very Slight - 5 : Very Intense')
+    st.caption('0 : None - 1 : Muted - 2 : Slight - 3 : Mild - 4 : Intense - 5 : Very Intense')
     with st.container():
         col1, col2 = st.columns(2)
         smoothness = col1.number_input('Smoothness', 0, 5)
