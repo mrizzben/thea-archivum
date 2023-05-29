3ce065f37816b76704ae0fcd1ede4efe7332e25e
Rizz <26541542+mrizzben@users.noreply.github.com>
2023-05-29 09:28:34 +0700

    feat: readjust app template

--- teascore.py
+++ teascore.py
@@ -1,19 +1,24 @@
 import streamlit as st
 
-flavor_attributes = {
-    'Sweetness': ['Honey', 'Sugar', 'Caramel', 'Maple Syrup'],
-    'Bitterness': ['Dark Chocolate', 'Coffee', 'Grapefruit Peel', 'Black Walnut'],
-    'Astringency': ['Green Apple', 'Black Tea', 'Cranberry', 'Unripe Persimmon'],
-    'Floral': ['Rose', 'Jasmine', 'Lavender', 'Honeysuckle'],
-    'Fruity': ['Berry', 'Citrus', 'Tropical', 'Stone Fruit'],
-    'Earthy': ['Mushroom', 'Forest Floor', 'Damp Earth', 'Wet Leaves'],
-    'Herbal': ['Mint', 'Basil', 'Thyme', 'Eucalyptus'],
-    'Spicy': ['Cinnamon', 'Ginger', 'Cloves', 'Peppercorn'],
-    'Vegetal': ['Grass', 'Seaweed', 'Artichoke', 'Fresh Herbs'],
-    'Nutty': ['Almond', 'Peanut', 'Hazelnut', 'Cashew'],
-    'Woody': ['Cedar', 'Oak', 'Pine', 'Sandalwood'],
-    'Other': ['Biscuit', 'Molasses', 'Tobacco', 'Leather']
+flavor_wheel = {
+    'Sweetness': ['Honey', 'Caramel', 'Sugar', 'Maple Syrup', 'Marshmallow', 'Agave'],
+    'Bitterness': ['Dark Chocolate', 'Coffee', 'Gentian', 'Quinine', 'Grapefruit Peel', 'Black Walnut'],
+    'Astringency': ['Pomegranate', 'Cranberry', 'Green Banana', 'Grape Skin', 'Black Tea', 'Walnut Skin'],
+    'Floral': ['Jasmine', 'Rose', 'Lavender', 'Chamomile', 'Osmanthus', 'Honeysuckle'],
+    'Fruity': ['Citrus', 'Berry', 'Stone Fruit', 'Tropical Fruit', 'Apple', 'Mango'],
+    'Earthy': ['Mushroom', 'Forest Floor', 'Damp Earth', 'Wet Leaves', 'Roasted Beet', 'Oak Moss'],
+    'Herbal': ['Mint', 'Basil', 'Thyme', 'Eucalyptus', 'Sage', 'Lemongrass'],
+    'Spicy': ['Cinnamon', 'Ginger', 'Cloves', 'Peppercorn', 'Cardamom', 'Nutmeg'],
+    'Vegetal': ['Grass', 'Seaweed', 'Artichoke', 'Fresh Herbs', 'Green Bell Pepper', 'Snow Pea'],
+    'Nutty': ['Almond', 'Peanut', 'Hazelnut', 'Cashew', 'Pistachio', 'Walnut'],
+    'Woody': ['Cedar', 'Oak', 'Pine', 'Sandalwood', 'Cypress', 'Mahogany'],
+    'Other': ['Biscuit', 'Molasses', 'Tobacco', 'Leather', 'Burnt Sugar', 'Toasted Sesame']
 }
+
+color = ['Pale/White', 'Light Green', 'Yellow', 'Orange', 'Brown', 'Dark Red']
+clarity = ['Clear', 'Dark', 'Murky']
+texture = ['Heavy', 'Metallic', 'Thick', 'Creamy', 'Full', 'Empty', 'Bright', 'Soft', 'Light']
+
 
 def main():
     st.title('Tea Tasting Score Sheet')
@@ -22,42 +27,48 @@ def main():
     date = st.date_input('Date')
     tea_name = st.text_input('Tea Name')
 
-    st.header('Dry Leaf Evaluation')
-    dry_leaf_appearance = st.text_input('Dry Appearance')
-    dry_leaf_aroma = st.text_input('Dry Aroma')
+    with st.container():
 
-    st.header('Wet Leaf Evaluation')
-    wet_leaf_appearance = st.text_input('Wet Appearance')
-    wet_leaf_aroma = st.text_input('Wet Aroma')
+        col1, col2 = st.columns(2)
+
+        col1.header('Dry Leaf Evaluation')
+        dry_leaf_appearance = col1.text_input('Dry Appearance')
+        dry_leaf_aroma = col1.text_input('Dry Aroma')
+
+        col2.header('Wet Leaf Evaluation')
+        wet_leaf_appearance = col2.text_input('Wet Appearance')
+        wet_leaf_aroma = col2.text_input('Wet Aroma')
 
     st.header('Liquor Evaluation')
-    liquor_appearance = st.text_input('Appearance (Color, Clarity)')
-    liquor_aroma = st.text_input('Aroma (Intensity, Quality)')
-    body = st.text_input('Body (Viscosity, Weight)')
-    overall_flavor = st.text_input('Flavor (Overall)')
+    liquor_color = st.select_slider('Liqour Color', color)
+    liqour_clarity = st.radio('Liqour Clarity', clarity)
+    liquor_intensity = st.slider('Aroma Intensity', 1, 5)
+    liqour_body = st.multiselect('Body (Viscosity, Weight)', texture)
 
     st.header('Flavor Intensity')
-    sweetness = st.number_input('Sweetness', min_value=1, max_value=10)
-    bitterness = st.number_input('Bitterness', min_value=1, max_value=10)
-    astringency = st.number_input('Astringency', min_value=1, max_value=10)
-    floral = st.number_input('Floral', min_value=1, max_value=10)
-    fruity = st.number_input('Fruity', min_value=1, max_value=10)
-    earthy = st.number_input('Earthy', min_value=1, max_value=10)
-    herbal = st.number_input('Herbal', min_value=1, max_value=10)
-    spicy = st.number_input('Spicy', min_value=1, max_value=10)
-    vegetal = st.number_input('Vegetal', min_value=1, max_value=10)
-    nutty = st.number_input('Nutty', min_value=1, max_value=10)
-    woody = st.number_input('Woody', min_value=1, max_value=10)
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+        sweetness = col1.number_input('Sweetness', min_value=1, max_value=10)
+        bitterness = col2.number_input('Bitterness', min_value=1, max_value=10)
+        astringency = col3.number_input('Astringency', min_value=1, max_value=10)
+        floral = col1.number_input('Floral', min_value=1, max_value=10)
+        fruity = col2.number_input('Fruity', min_value=1, max_value=10)
+        earthy = col3.number_input('Earthy', min_value=1, max_value=10)
+        herbal = col1.number_input('Herbal', min_value=1, max_value=10)
+        spicy = col2.number_input('Spicy', min_value=1, max_value=10)
+        vegetal = col3.number_input('Vegetal', min_value=1, max_value=10)
+        nutty = col1.number_input('Nutty', min_value=1, max_value=10)
+        woody = col2.number_input('Woody', min_value=1, max_value=10)
 
     selected_flavors = {}
 
-    st.title('Flavour Attributes')
+    st.header('Flavour Attributes')
 
-    for attribute, options in flavor_attributes.items():
+    for attribute, options in flavor_wheel.items():
         selected = st.multiselect(attribute, options)
         selected_flavors[attribute] = selected
 
-    st.title('Selected Flavors')
+    st.header('Selected Flavors')
     st.write(selected_flavors)
 
     st.header('Aftertaste')
