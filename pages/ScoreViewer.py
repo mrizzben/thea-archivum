1fd238303d607964f0844e896765182fa4d5c897
Rizz <26541542+mrizzben@users.noreply.github.com>
2023-09-06 17:16:14 +0700

    ✨ feat(ScoreViewer):
    add flavour wheel in ScoreViewer

--- pages/ScoreViewer.py
+++ pages/ScoreViewer.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import plotly.express as px
 
-keys = [x for x in st.session_state.keys() if isinstance(x, str)]
+keys = [
+    x for x in st.session_state.keys() if isinstance(x, str) and "_flavours" not in x
+]
 if keys:
-    scoresheet = st.selectbox(
-        "Which score sheet you would like to view?", st.session_state.keys()
-    )
+    scoresheet = st.selectbox("Which score sheet you would like to view?", keys)
+    with st.expander("Flavour Chart", expanded=True):
+        fig = px.line_polar(
+            r=st.session_state[scoresheet + "_flavours"].values(),
+            theta=st.session_state[scoresheet + "_flavours"].keys(),
+            line_close=True,
+        )
+        fig.update_traces(fill="toself")
+        st.plotly_chart(fig)
     st.dataframe(st.session_state[scoresheet])
 
 else:
