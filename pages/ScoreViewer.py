bf9253e002e47b4787028d18931c65358fd9a88d
Rizz <26541542+mrizzben@users.noreply.github.com>
2023-09-06 17:26:21 +0700

    🐞 fix(ScoreViewer):
    ScoreViewer session_state leakage

--- pages/ScoreViewer.py
+++ pages/ScoreViewer.py
@@ -1,20 +1,22 @@
 import streamlit as st
 import plotly.express as px
 
-keys = [
-    x for x in st.session_state.keys() if isinstance(x, str) and "_flavours" not in x
-]
-if keys:
-    scoresheet = st.selectbox("Which score sheet you would like to view?", keys)
-    with st.expander("Flavour Chart", expanded=True):
-        fig = px.line_polar(
-            r=st.session_state[scoresheet + "_flavours"].values(),
-            theta=st.session_state[scoresheet + "_flavours"].keys(),
-            line_close=True,
+if st.session_state.keys():
+    keys = [x for x in st.session_state.keys() if isinstance(x, str)]
+    if keys:
+        scoresheet = st.selectbox(
+            "Which score sheet you would like to view?",
+            [x for x in keys if "_flavours" not in x],
         )
-        fig.update_traces(fill="toself")
-        st.plotly_chart(fig)
-    st.dataframe(st.session_state[scoresheet])
+        with st.expander("Flavour Chart", expanded=True):
+            fig = px.line_polar(
+                r=st.session_state[scoresheet + "_flavours"].values(),
+                theta=st.session_state[scoresheet + "_flavours"].keys(),
+                line_close=True,
+            )
+            fig.update_traces(fill="toself")
+            st.plotly_chart(fig)
+        st.dataframe(st.session_state[scoresheet])
 
 else:
     st.info("You've not filled any score sheet yet!")
