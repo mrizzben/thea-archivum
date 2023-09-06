1ed2ff81b14d063f67a58ad7f21542c42399422b
Rizz <26541542+mrizzben@users.noreply.github.com>
2023-09-06 17:03:22 +0700

    ✨ feat(ScoreViewer):
    add a message if no scoresheet is available to show

--- pages/ScoreViewer.py
+++ pages/ScoreViewer.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-if isinstance(st.session_state.keys(), str):
-    keys = [x for x in st.session_state.keys() if isinstance(x, str)]
+keys = [x for x in st.session_state.keys() if isinstance(x, str)]
+if keys:
     scoresheet = st.selectbox(
         "Which score sheet you would like to view?", st.session_state.keys()
     )
     st.dataframe(st.session_state[scoresheet])
+
+else:
+    st.info("You've not filled any score sheet yet!")
